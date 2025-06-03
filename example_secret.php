d3b5b8ec15f8730e3daaed05b295727a71fb3b82
labtestsubb <152941466+labtestsubb@users.noreply.github.com>
2025-06-03 13:33:38 +0800

    Create example_secret.php

---
+++ example_secret.php
@@ -0,0 +1,6 @@
+<?php
+$githubToken = "ghp_abcdEfghIjklMnopQRStuvwxYZ1234567890"; // GitHub token (detected)
+$dbPassword = "MySQLPass@123";                               // DB password (detected)
+
+echo "Secrets should not be hardcoded!";
+?>
